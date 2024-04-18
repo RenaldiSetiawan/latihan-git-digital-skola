2ea15be4de02cffbe53636b0585bef7b0e12cd9d
renaldisetiawan <enang.setiawan22@gmail.com>
2024-04-18 17:39:03 +0700

    Latihan Git

---
+++ test.js
@@ -0,0 +1,16 @@
+/* Git Command
+- git add       
+- git commit   
+- git push      
+- git fetch 
+- git pul
+- git merge 
+- git log 
+- git revert 
+- git reset 
+
+Create Branch
+- git branch                   : Untuk melihat list branch
+- git checkout <nama-branch>   : Untuk berpindah ke branch tertentu
+- git checkout -b <nama-branch : membuat sebuah branch baru */
+console.log("Latihan Git");
